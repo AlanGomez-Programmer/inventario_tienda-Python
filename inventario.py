86d01243e062aa3fac68135b3a5465f93a54abd0
AlanGomez-Programmer <Alan.s.g.j.2006@gmail.com>
2026-04-23 17:18:46 -0600

    feat: :sparkles: Se agrega la función agregar productos
    Se crea la función agregar productos, ademas se crean funciones para la validaciones del nuevo producto

--- inventario.py
+++ inventario.py
@@ -1,11 +1,42 @@
-from utilidades import mostar_separadores, pedir_num_enteros
+from utilidades import ( 
+    mostar_separadores, 
+    pedir_num_enteros, 
+    ruta_inventario, 
+    leer_archivo, 
+    guardar_datos,
+    validar_producto, 
+    pedir_precio, 
+    pedir_cantidad
+) 
 
 def menu():
+    while True:
+        mostar_separadores()
+        print("--- MENU DE INVENTARIO ---")
+        print("1. Agregar Producto")
+        print("2. Listar Productos")
+        print("3. Actualizar Cantidad")
+        print("4. Eliminar Producto")
+        print("5. Calcular Valor Total del Inventario")
+        opci = pedir_num_enteros("Ingrese una opción: ")
+
+        if opci == 1:
+            agregar_producto()
+
+def agregar_producto():
+    productos = leer_archivo(ruta_inventario)
     mostar_separadores()
-    print("--- MENU DE INVENTARIO ---")
-    print("1. Agregar Producto")
-    print("2. Listar Productos")
-    print("3. Actualizar Cantidad")
-    print("4. Eliminar Producto")
-    print("5. Calcular Valor Total del Inventario")
-    opci = pedir_num_enteros("Ingrese una opción: ")
+    print("--- REGISTRO DE PRODUCTO ---")
+    nombre_producto = validar_producto()
+    precio = pedir_precio()
+    cantidad = pedir_cantidad()
+
+    producto = {
+        "precio": precio,
+        "cantidad": cantidad
+    }
+    
+    productos[nombre_producto] = producto
+
+    guardar_datos(ruta_inventario, productos)
+    print(f"\033[033Producto Registrado exitosamente \033[0m")
--- utilidades.py
+++ utilidades.py
@@ -1,9 +1,35 @@
 import os
+from json import load, dumps, dump
 
 def mostar_separadores():
     os.system("clear")
     print("="*40)
 
+# FUNCIONES PARA LA PERSISTENCIA DE DATOS
+
+ruta_inventario = "inventario.json"
+
+def leer_archivo(ruta):
+    try:
+        with open(ruta, "r") as archivo:
+            datos = load(archivo)
+            return datos
+    except FileNotFoundError:
+        print("\033[31mERROR: No existe el archivo \033[0m")
+        
+        with open(ruta, "w") as archivo:
+            dump({}, archivo)
+            print("\033[34mSe ha creado un archivo llamado inventario.json\033[0m")
+            return {}
+    
+def guardar_datos(ruta, datos):
+    try:
+        with open(ruta, "w") as archivo:
+            archivo.write(dumps(datos, indent= 4))
+    except Exception:
+        print("\033[31mError inesperado al guardar \033[0m")
+
+# VALIDACIONES
 
 def pedir_num_enteros(mensaje):
     while True:
@@ -17,3 +43,56 @@ def pedir_num_enteros(mensaje):
                 return numero
         except ValueError:
             print("\033[31mERROR: Solo se aceptan numeros enteros \033[0m")
+
+def pedir_letras(mensaje):
+    while True:
+        texto = input(mensaje).strip()
+        
+        solo_letras = all(c.isalpha() or c.isspace() for c in texto)
+
+        if not solo_letras:
+            print(f"\033[031mERROR: Solo se permiten letras \033[0m")
+            continue
+
+        if texto == "":
+            print(f"\033[031mERROR: No se permite dejar en blanco este apartado \033[0m")
+            continue
+
+        if texto:
+            return texto.title()
+
+def validar_producto():
+    productos = leer_archivo(ruta_inventario)
+    while True:
+        producto = pedir_letras("PRODUCTO: ").strip().capitalize()
+        
+        if producto in productos:
+            print(f"\033[031mERROR: Ese producto ya existe \033[0m")
+        else:
+            return producto
+
+def pedir_precio():
+    while True:
+        try:
+            precio = float(input("PRECIO: "))
+            
+            if precio <= 0:
+                print(f"\033[031mERROR: El precio degbe ser mayor a 0 \033[0m")
+                continue
+            else:
+                return precio
+        except ValueError:
+            print(f"\033[031mERROR: Solo se permiten numeros decimales \033[0m")
+
+def pedir_cantidad():
+  while True:
+        try:
+            cantidad = int(input("CANTIDAD: "))
+            
+            if cantidad <= 0:
+                print(f"\033[031mERROR: La cantidad de productos debe se mayor a 0 \033[0m")
+                continue
+            else:
+                return cantidad
+        except ValueError:
+            print(f"\033[031mERROR: Solo se permiten numeros enteros \033[0m")
